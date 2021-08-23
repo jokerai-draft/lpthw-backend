915dc1c69d28fad6be0f7448fad4a65afe231d19
noname <you@example.com>
2021-08-23 20:06:00 +0800

    layer missing detection

---
+++ ex3/S2d/project13/Hub1.php
@@ -0,0 +1,48 @@
+<?php
+class Hub1
+{
+    public static $state;
+
+    public static function start() {
+        Assembled1::init();
+        Component1::init();
+
+        // self::loginCheck();
+
+        self::getState();
+
+        self::perform();
+
+    }
+
+    public static function loginCheck() {
+        // echo "login please";
+        // exit();
+        SessionedComponent::init();
+        SessionedComponent::onNotif();
+        if (SessionedComponent::$state['isLoggedIn'] !== true) {
+            // StateSessionService::tryLogin(); // bypass
+            // echo "please login";
+            // better to redirct to "document.login.php/login.php" as it renders view('view.login.php'); // to trigger tryLogin()
+            // exit();
+            header("Location: ./document.login.php?location=document1.php");
+            exit();
+        }
+        if (SessionedComponent::$state['isLoggedIn'] === true) {
+            // StateSessionService::logout();
+        }
+    }
+
+    public static function getState() {
+        Component1::onNotif();
+        self::$state = Component1::$state; // 关键值的获取(此两行)
+    }
+
+    public static function perform() {
+        $level1payload = [];
+        $level1payload = self::$state; // Assembled1 并不知道 state's component 的存在
+        Assembled1::performIn($level1payload);
+        Assembled1::performOut(); // 处理从 inbound 到 outbound
+    }
+
+}
---
+++ ex3/S2d/project13/view1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document 1</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: row;
+        }
+        .container > div {
+            margin: 30px 30px;
+        }
+    </style>
+</head>
+<body>
+    <h4>Hi <?php echo $httpMessageHandler['GET']['name'] ?? "my friend" ?></h4>
+    <p><?php echo (new \DateTime())->format('Y-m-d H:i:s'); ?></p>
+
+<?php
+$url = $httpMessageHandler['URL'];
+// $result1 = $databag['level1payload']['result1'];
+// $writtingTimes1 = $databag['level1payload']['writtingTimes1'];
+// $result2 = $databag['level1payload']['result2'];
+// $writtingTimes2 = $databag['level1payload']['writtingTimes2'];
+$result1 = $databag['level1payload'][0]['result'];
+$writtingTimes1 = $databag['level1payload'][0]['writtingTimes'];
+$result2 = $databag['level1payload'][1]['result'];
+$writtingTimes2 = $databag['level1payload'][1]['writtingTimes'];
+?>
+<div class="container">
+    <div>
+        <h4>Result 1</h4>
+        <p>value for current time interval: <?= $result1 ?></p>
+        <p>written <?= $writtingTimes1 ?> times</p>
+    </div>
+    <div>
+        <h4>Result 2</h4>
+        <p>value for current time interval: <?= $result2 ?></p>
+        <p>written <?= $writtingTimes2 ?> times</p>
+    </div>
+</div>
+<span>by <?= $url ?></span>
+
+</body>
+</html>
+<!--
+http://localhost:8000/document1.php
+http://localhost:8000/document1.php?name=tom
+http://localhost:8000/document1.php"><script>alert(‘xss’);</script>
+
+http://localhost:8000/document1.php?name=tom&keyword=you"><script>alert('XSS');</script>
+-->
